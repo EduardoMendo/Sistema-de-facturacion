329dc662b174850a19391f56ff949262de05e465
EduardoMendo <efmendoza@uamv.edu.ni>
2024-10-08 18:36:54 -0600

    modifique Iclient

--- facturacion/src/main/java/interfaces/IClient.java
+++ facturacion/src/main/java/interfaces/IClient.java
@@ -7,12 +7,8 @@ import java.util.List;
 public interface IClient {
 
     public void save(Client client);
-    public Client getClient(int id);
     public void UpdateClient(Client client);
     public void delete(Client client);
     public List<Client> getAllClient();
-
-
-
 
 }
